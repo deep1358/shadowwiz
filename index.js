b48896f0950103f9ff0599b055a5cf5a52f03439
Deep Shah <deepshah1358@gmail.com>
2021-09-07 13:38:11 +0530

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const shadowwiz = (options) => {
   else options.shadow_type = '15px';
 
   imgs.forEach(img => {
-    img.style.boxshadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
+    img.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`
 
     if (options.padding)
       img.style.padding = '1em';
